50f23fff3e9304eccf7c4e15003e680f356d3c60
lizeyliz <alizaeaton@gmail.com>
2025-02-08 15:07:47 -0800

    minor changes

--- AlizaMethods.java
+++ AlizaMethods.java
@@ -4,13 +4,15 @@ public class AlizaMethods {
     DatabaseMethods database = new DatabaseMethods();
     //sort the tree
     //takes in root of tree
+    //UNFINISHED
     public void sortTree(DatabaseNode root) {
         //traverse tree inorder and nodes in array
         inOrderArray(root);
         //make a new tree with the array and return it (replace old tree)
     }//end sortTree
 
-
+    //UNTESTED
+    //iterative inorder traversal, returns array of nodes
     public DatabaseNode[] inorderArray(DatabaseNode root) {
         DatabaseNode[] treeArray = new DatabaseNode[database.countRecords(root)];
         int index = 0;
@@ -34,27 +36,5 @@ public class AlizaMethods {
         }
         return treeArray;
     }//end inorderArray
-
-    //recursive method: creates array of nodes from inorder traversal of tree
-    //takes in root of tree
-    //helper method for sortTree
-    /*public DatabaseNode[] inOrderArray(DatabaseNode node, int index) { // INORDER TRAVERSAL
-        //create a an array that is the size of the number of records in the tree
-        //to store the nodes in the tree
-        DatabaseNode[] treeArray = new DatabaseNode[database.countRecords(node)];
-
-        // left tree
-        inOrderArray(node.getLeftChild(), index);
-
-        //add the current node to the array
-        treeArray[index] = node;
-        index++;
-        // right tree
-        inOrderArray(node.getRightChild(), index);
-
-        return treeArray;
-    }// end inOrderArray*/
-
-
 }//end AlizaMethods
 
