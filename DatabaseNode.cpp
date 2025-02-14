017c721e620fcdc4f7926d050278e734f35d76e3
lizeyliz <alizaeaton@gmail.com>
2025-02-14 09:53:38 -0800

    started cpp

---
+++ DatabaseNode.cpp
@@ -0,0 +1,23 @@
+#include <string>
+class DatabaseNode {
+    private:
+    //instance variables
+    int idNum; //9 digit
+    std::string firstName;
+    std::string lastName;
+    std::string address;
+    std::string city;
+    std::string state;
+    int zip;
+    std::string email;
+    std::string phNum;
+
+    public:
+    //pointing to left and right
+    DatabaseNode *left, *right;
+    //constructor
+    DatabaseNode(int idNum, std::string firstName, std::string lastName, std::string address, 
+    std::string city, std::string state, int zip, std::string email, std::string phNum){
+        
+    }
+};
